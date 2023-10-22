4bf5d88b6c301dae549edf1c771ede925c860fae
Camille Kaniecki <ckaniecki@acvauctions.com>
2023-10-22 10:32:47 -0400

    basic app setup

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+export default defineNuxtConfig({
+  //...
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
